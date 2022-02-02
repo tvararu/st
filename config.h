b9782b3cc5088f0a13a1973c08c73cd4f1d643c5
Theodor Vararu <theo@vararu.org>
2022-02-02 18:43:06 +0000

    Add newterm patch
    https://st.suckless.org/patches/newterm/

--- config.h
+++ config.h
@@ -263,6 +263,7 @@ static Shortcut shortcuts[] = {
 	{ MODKEY,               XK_v,           clippaste,      {.i =  0} },
 	{ ShiftMask,            XK_Insert,      selpaste,       {.i =  0} },
 	{ TERMMOD,              XK_Num_Lock,    numlock,        {.i =  0} },
+	{ TERMMOD,              XK_Return,      newterm,        {.i =  0} },
 	{ ShiftMask,            XK_Page_Up,     kscrollup,      {.i = -1} },
 	{ ShiftMask,            XK_Page_Down,   kscrolldown,    {.i = -1} },
 	{ MODKEY,               XK_Page_Up,     kscrollup,      {.i = -1} },
